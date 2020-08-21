8efe1c0f12e00c12257d928f57c0b233b97a0e02
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2020-08-20 23:14:41 -0300

    chore(view): remove error container from layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,19 +67,6 @@
       </div>
       @endisset
 
-      @if($errors->any())
-      <div class="container">
-        <div class="alert alert-danger" role="alert">
-          <ul class="mb-0">
-            @foreach($errors->all() as $error)
-            <li>
-              {!! $error !!}
-            </li>
-            @endforeach
-          </ul>
-        </div>
-      </div>
-      @endif
       @yield('content')
     </main>
   </div>
