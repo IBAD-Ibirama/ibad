01b822ea6234afd5d8febedc296af1dac475c519
João Arruda <jvanunes1@gmail.com>
2020-08-24 22:37:55 -0300

    style: adaptar estilos para os indices corporais

--- resources/views/athlete_evaluation/create.blade.php
+++ resources/views/athlete_evaluation/create.blade.php
@@ -1,31 +1,85 @@
 <style>
+    * {
+        font-family: Arial, Helvetica, sans-serif;
+    }
     label, input:not([hidden]) {
         display: block;
     }
+    #formContainer {
+        padding: 30px;
+        border: 1px solid #cccc;
+        width: fit-content;
+        border-radius: 10px;
+    }
+    #container{
+        margin-top: 20px;
+        display: flex;
+        justify-content:center;
+    }
+    input, select{
+        margin-bottom: 10px;
+        height: 30px;
+        font-size: 15px;
+        width: 100%;
+        border-radius: 10px;
+        border: 1px solid #cccc;
+        padding-left: 5px;
+    }
+    input, select:focus{
+        outline: none;
+    }
+    label{
+        margin-bottom: 2px;
+    }
+    .button{
+        margin-top: 20px;
+        padding: 5px;
+        height: 30px;
+        width: 100%;
+    }
+    input[type="submit"] {
+        cursor: pointer;
+    }
+    input[type="submit"]:hover {
+        opacity: 0.9;
+    }
 </style>
-<form action="{{ route('avaliacao_atleta.store') }}" method="POST">
-    @csrf
-    <label>Atleta</label>
-    <select name="athlete_id" required>
-        @foreach ($athletes as $athlete)
-            <option value="{{ $athlete->id }}">
-                {{ $athlete->name }}
-            </option>
-        @endforeach
-    </select>
-    <label>Data da Avaliação</label>
-    <input type="date" name="realization_date" required/>
-    Testes Físicos:
-    @foreach ($physicalTests as $physicalTest)
-        <label>{{ $physicalTest->name }}</label>
-        <input type="number" name="physical_tests[]" value="{{ $physicalTest->id }}" hidden/>
-        <input type="number" name="physical_tests_values[]"/>
-    @endforeach
-    Índices Corporais:
-    @foreach ($bodyIndexes as $bodyIndex)
-        <label>{{ $bodyIndex->name }}</label>
-        <input type="number" name="body_indexes[]" value="{{ $bodyIndex->id }}" hidden/>
-        <input type="number" name="body_indexes_values[]"/>
-    @endforeach
-    <input type="submit" name="Salvar"/>
-</form>
+
+<div id="container">
+    <div id="formContainer">
+        <form action="{{ route('avaliacao_atleta.store') }}" method="POST">
+            @csrf
+            <div>
+                <h4>Avaliação:</h4>
+                <label>Atleta</label>
+                <select name="athlete_id" required>
+                    <option value="">Selecione um atleta</option>
+                    @foreach ($athletes as $athlete)
+                        <option value="{{ $athlete->id }}">
+                            {{ $athlete->name }}
+                        </option>
+                    @endforeach
+                </select>
+                <label>Data da Avaliação</label>
+                <input type="date" name="realization_date" required/>
+            </div>
+            <div>
+                <h4>Testes Físicos:</h4>
+                @foreach ($physicalTests as $physicalTest)
+                    <label>{{ $physicalTest->name }}</label>
+                    <input type="number" name="physical_tests[]" value="{{ $physicalTest->id }}" hidden/>
+                    <input type="number" name="physical_tests_values[]"/>
+                @endforeach
+            </div>
+            <div>
+                <h4>Índices Corporais:</h4>
+                @foreach ($bodyIndexes as $bodyIndex)
+                    <label>{{ $bodyIndex->name }}</label>
+                    <input type="number" name="body_indexes[]" value="{{ $bodyIndex->id }}" hidden/>
+                    <input type="number" name="body_indexes_values[]"/>
+                @endforeach
+            </div>
+            <input type="submit" name="Salvar" class="button"/>
+        </form>
+    </div>
+</div>
--- resources/views/athlete_evaluation/index.blade.php
+++ resources/views/athlete_evaluation/index.blade.php
@@ -1,20 +1,95 @@
-<a href="{{ route('avaliacao_atleta.create') }}">Novo</a>
-@foreach ($athleteEvaluations as $athleteEvaluation)
-    <div>
-        Atleta: {{ $athleteEvaluation->athlete->name }} <br/>
-        Data: {{ $athleteEvaluation->realization_date }}
-        <div>
-            Testes Físicos:
-            @foreach ($athleteEvaluation->physicalTests as $athleteEvaluationPhysicalTest)
-                {{ $athleteEvaluationPhysicalTest->physicalTest->name }}: {{ $athleteEvaluationPhysicalTest->value }} <br/>
+<style>
+    * {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    a{
+        border: 1px solid #cccc;
+        padding: 10px;
+        border-radius: 5px;
+        color: #000;
+        text-decoration: none;
+    }
+    a:hover {
+        background: #cccc;
+    }
+    tr:nth-child(even) {
+        background-color: #f0f0f0;
+    }
+    #container{
+        margin-top: 50px;
+    }
+    #tabela{
+        border-collapse: collapse;
+        text-align: center;
+    }
+    th{
+        background: #cccc;
+    }
+    td, th{
+        border: 1px solid #cccc;
+        padding: 5px;
+        min-width: 100px;
+    }
+    #tabelaContainer{
+        margin-top: 30px;
+    }
+</style>
+
+<div id="container">
+    <a href="{{ route('avaliacao_atleta.create') }}">Novo</a>
+</div>
+
+<div id="tabelaContainer">
+    <table id="tabela">
+        <theader>
+            <tr>
+                <th width="20%">
+                    Atleta
+                </th>
+                <th width="10%">
+                    Data
+                </th>
+                <th width="35%">
+                    Testes Físicos
+                </th>
+                <th width="35%">
+                    Índices Corporais
+                </th>
+            </tr>
+        </theader>
+        <tbody>
+            @foreach ($athleteEvaluations as $athleteEvaluation)
+                <tr>
+                    <td>
+                        {{ $athleteEvaluation->athlete->name }}
+                    </td>
+                    <td>
+                        {{ $athleteEvaluation->realization_date }}
+                    </td>
+                    <td>
+                        @if ($athleteEvaluation->physicalTests()->count() > 0)
+                            @foreach ($athleteEvaluation->physicalTests as $athleteEvaluationPhysicalTest)
+                                <div>
+                                    {{ $athleteEvaluationPhysicalTest->physicalTest->name }}: {{ $athleteEvaluationPhysicalTest->value }}
+                                </div>
+                            @endforeach
+                        @else
+                            Não informado
+                        @endif
+                    </td>
+                    <td>
+                        @if ($athleteEvaluation->bodyIndexes()->count() > 0) 
+                            @foreach ($athleteEvaluation->bodyIndexes as $athleteEvaluationBodyIndex)
+                                <div>
+                                    {{ $athleteEvaluationBodyIndex->bodyIndex->name }}: {{ $athleteEvaluationBodyIndex->value }} <br/>
+                                </div>
+                            @endforeach
+                        @else
+                            Não informado                            
+                        @endif
+                    </td>
+                </tr>
             @endforeach
-        </div>
-        <div>
-            Índices Corporais:
-            @foreach ($athleteEvaluation->bodyIndexes as $athleteEvaluationBodyIndex)
-                {{ $athleteEvaluationBodyIndex->bodyIndex->name }}: {{ $athleteEvaluationBodyIndex->value }} <br/>
-            @endforeach
-        </div>
-    </div>
-    <hr>
-@endforeach
+        </tbody>
+    </table>
+</div>
