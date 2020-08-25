0d161c28e6c0dc90f5344f5f2589f26a62c4d86a
Volpe6 <baasch2014@gmail.com>
2020-08-25 15:37:54 -0300

    refect: ajusta o cadastro do registro de competição

--- resources/views/atletas/cadastroRegistroCompeticao.blade.php
+++ resources/views/atletas/cadastroRegistroCompeticao.blade.php
@@ -22,15 +22,15 @@
                     </div>
                     <div class="form-group">
                         <label for="coordinator">Coordenador Competição</label>
-                        <input type="text" class="form-control" id="coordinator" name="coordinator" placeholder="Coordenador da competição">
+                        <input type="text" class="form-control" id="coordinator" name="coordinator" placeholder="Coordenador da competição" readonly>
                     </div>
                     <div class="form-group">
                         <label for="competition_level">Nível Competição</label>
-                        <input type="text" class="form-control" id="competition_level" name="competition_level" placeholder="Nivel competição">
+                        <input type="text" class="form-control" id="competition_level" name="competition_level" placeholder="Nivel competição" readonly>
                     </div>
                     <div class="form-group">
                         <label for="date">Data Competição</label>
-                        <input type="text" class="form-control" id="date" name="date" placeholder="data da competição">
+                        <input type="text" class="form-control" id="date" name="date" placeholder="data da competição" readonly>
                     </div>
                 </div>
                 <div class="col">
