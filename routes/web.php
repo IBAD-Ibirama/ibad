b16736230312e646e861a12a556dc1abc422d38a
ipdmartins <ipdmartins@gmail.com>
2020-08-24 22:26:50 -0300

    refactor: adding sponsors route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 Route::get('/', function () {
     return redirect('/login');
@@ -11,3 +13,7 @@ Auth::routes();
 Route::resource('usuarios', 'UserController');
 
 Route::get('/dashboard', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('sponsors', 'SponsorController');
+});
