bdccaa83825ea923b3fd5589ef08c9dbf763b695
Lucas Fusinato Wilhelm Chiodini Zanis <lucasfusinato-17@hotmail.com>
2020-09-07 23:21:14 -0300

    feat: plano de atividades para treinos

---
+++ app/Http/Controllers/PlanningController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Planning;
+use App\Training;
+use App\Team;
+use App\Http\Requests\PlanningForm;
+use Illuminate\Http\Request;
+
+class PlanningController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \App\Team      $team
+     * @param  \App\Training  $training
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Team $team, Training $training)
+    {
+        $plannings = $training->plannings()->get()->sortBy('date');
+        return view('plannings.index', compact('team', 'training', 'plannings'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param  \App\Team      $team
+     * @param  \App\Training  $training
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Team $team, Training $training)
+    {
+        return $this->edit($team, $training, new Planning());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\PlanningForm  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PlanningForm $request)
+    {
+        return $this->update($request, new Planning());
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Team      $team
+     * @param  \App\Training  $training
+     * @param  \App\Planning  $planning
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Team $team, Training $training, Planning $planning)
+    {
+        return view('plannings.edit', compact('team', 'training', 'planning'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\PlanningForm  $request
+     * @param  \App\Planning  $planning
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PlanningForm $request, Planning $planning)
+    {
+        $request->persist($planning);
+        return redirect()->route('plannings.index', ['team' => $planning->training->team, 'training' => $planning->training]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Planning  $planning
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Planning $planning)
+    {
+        $team = $planning->training->team;
+        $training = $planning->training;
+        $planning->delete();
+        return redirect()->route('plannings.index', compact('team', 'training'));
+    }
+}
---
+++ app/Http/Requests/PlanningForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Planning;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PlanningForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'training_id' => 'required|numeric',
+            'name' => 'required|max:50',
+            'description' => 'required|max:200'
+        ];
+    }
+
+    /**
+     * Persists planning's data.
+     * 
+     * @param \App\Planning $planning
+     */
+    public function persist(Planning $planning)
+    {
+        $planning->training_id = $this->training_id;
+        $planning->name = $this->name;
+        $planning->description = $this->description;
+
+        $planning->save();
+    }
+}
