cfe6a12131441bd8db5e7cc90600f4547e441404
ipdmartins <ipdmartins@gmail.com>
2020-09-12 19:22:25 -0300

    chore: building athlete factory attributes

--- database/factories/AthleteFactory.php
+++ database/factories/AthleteFactory.php
@@ -7,6 +7,16 @@ use Faker\Generator as Faker;
 
 $factory->define(Athlete::class, function (Faker $faker) {
     return [
-        //
+        'birthdate' => $faker->date(),
+        'gender' => 'm',
+        'rg' => $faker->randomNumber($nbDigits = 9, $strict = false),
+        'telephone' => $faker->creditCardNumber,
+        'shift' => 'm',
+        'grade' => $faker->word,
+        'health_problem' => $faker->word,
+        'medication' => $faker->word,
+        'cloth_size' => 'mm',
+        'blood_type' => 'o+pmm',
+        'school' => $faker->word,
     ];
 });
