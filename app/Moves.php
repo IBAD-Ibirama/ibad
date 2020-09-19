a1d7460e8795cb8cacfd560f4a5ed7b5b50cc444
Mario Fronza <mariofronza1@gmail.com>
2020-09-19 16:57:03 -0300

    feat: added user id to moves model

--- app/Moves.php
+++ app/Moves.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Moves extends Model
 {
     protected $fillable = [
-        'description', 'date', 'value', 'type', 'specification'
+        'description', 'date', 'value', 'type', 'specification', 'user_id'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
