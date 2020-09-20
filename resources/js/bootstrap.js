fce4e5a82e99283f0ea8793d3d63a0cdbbd9ed92
Lucas Fusinato Wilhelm Chiodini Zanis <lucasfusinato-17@hotmail.com>
2020-09-19 21:57:43 -0300

    chore: padrao laravel/ui para arquivos frontend #17

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,17 @@
-window._ = require("lodash");
+window._ = require('lodash');
+
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,9 +19,9 @@ window._ = require("lodash");
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require("axios");
+window.axios = require('axios');
 
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -26,5 +39,3 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
-
-try {   window.Popper = require('popper.js').default;   window.$ = window.jQuery = require('jquery');   require('bootstrap');​require('lightbox2');} catch (e) {}
