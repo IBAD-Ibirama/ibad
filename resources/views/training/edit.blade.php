a6c80ee87db4eaa3c6a26686d4cb569a893db3a8
Gustavo Jung <udesc.gustavo@gmail.com>
2020-09-27 18:19:34 -0300

    refactor: js codes in view files moved to a separeted folder

--- resources/views/training/edit.blade.php
+++ resources/views/training/edit.blade.php
@@ -227,92 +227,10 @@
 @endsection
 
 @section('script')
+
 <script>
-    const inputLocal = document.querySelector('#training_local');
-  const optionLocal = document.querySelector('#localOption');
-  const locals = {!! json_encode($place) !!};
-
-  const auxiliaryForm = document.querySelector('#form-auxiliarys');
-  const auxiliaryTitle = document.querySelector('#titleAuxiliary');
-  const team = document.querySelector('#teamOption');
-  const teams_can_have_auxiliary = {!!json_encode($teams_can_have_auxiliary)!!};
-  const auxiliary1 = document.querySelector('#auxiliary1');
-  const auxiliary2 =document.querySelector('#auxiliary2');
-
-  const initPeriodo = document.querySelector('#training_init');
-  const diaDaSemana = document.querySelector('#day_select');
-  const diaDaSemanaView = document.querySelector('#day_select_view');
-  const numTreino = document.querySelector("#num_treino");
-
-  inputLocal.onchange = () => {
-      optionLocal.selectedIndex = "0";
-  }
-
-  initPeriodo.onchange = setDay;
-
-  function setDay() {
-      try {
-        const [year, month, day] = initPeriodo.value.split('-');
-        const date = new Date(year, month-1, day);
-        const day2 = getDay(date.getDay());
-        diaDaSemanaView.value = day2[0];
-        diaDaSemana.value = day2[1];
-      } catch (error) {
-          console.log(error);
-      }
-  }
-
-setDay();
-
-  function getDay(index){
-      switch (index){
-        case 1:
-            return ['Segunda-Feira', 'mo']
-        case 2:
-            return ['Terça-Feira', 'tu']
-        case 3:
-            return ['Quarta-Feira', 'we']
-        case 4:
-            return ['Quinta-Feira', 'th']
-        case 5:
-            return ['Sexta-Feira', 'fr']
-        case 6:
-            return ['Sábado', 'sa']
-        case 0:
-            return ['Domingo', 'su']
-      }
-  }
-
-  function handleAuxiliaries(id){
-    if(!id){
-        auxiliaryForm.classList.remove('hide');
-        auxiliaryTitle.textContent = "Ajudantes:";
-        return;
-    }
-    const teamNeedAuxiliary = teams_can_have_auxiliary.find(team => team.id == id);
-    if(!teamNeedAuxiliary){
-        auxiliary1.value= null;
-        auxiliary2.value= null;
-        auxiliaryTitle.textContent = "Essa turma não necessita ajudantes!";
-        auxiliaryForm.classList.add('hide');
-    } else {
-        auxiliaryForm.classList.remove('hide');
-        auxiliaryTitle.textContent = "Ajudantes:";
-    }
-  }
-
-  team.onchange = () => {
-    handleAuxiliaries(team.value);
-  }
-
-  handleAuxiliaries(team.value);
-  optionLocal.onchange = () => {
-      if(!optionLocal.value) {
-          return;
-      }
-      const local = locals.find(local => local.id == optionLocal.value);
-      inputLocal.value = local.description;
-  }
-
+var locals = {!! json_encode($place) !!};
+var teams_can_have_auxiliary = {!!json_encode($teams_can_have_auxiliary)!!};
 </script>
+<script  src="{{asset('js/pages/training_edit.js')}}"></script>
 @endsection
